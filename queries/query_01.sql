d25c6b78e8bbe12c447cd79f69fd335559c7cfce
Jahid Hiron <jahidul@ewwfl.com>
2026-06-15 12:39:23 +0600

    query 1 is created

---
+++ queries/query_01.sql
@@ -0,0 +1,11 @@
+-- =============
+-- Query 1:
+-- =============
+
+SELECT
+    match_id,
+    fixture,
+    base_ticket_price
+FROM Matches
+WHERE tournament_category = 'Champions League'
+AND match_status = 'Available';
